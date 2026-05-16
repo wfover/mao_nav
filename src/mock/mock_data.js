616ceff04e64d9ad1f6a0561fa8f69aceb091d88
于先森（Mr.Yu） <yudehai0204@gmail.com>
2026-05-16 18:58:00 +0800

    chore: 更新导航数据 - 2026/5/16 18:57:59

--- src/mock/mock_data.js
+++ src/mock/mock_data.js
@@ -7,6 +7,13 @@ export const mockData = {
       "order": 0,
       "sites": [
         {
+          "id": "site-1774601326477",
+          "name": "猫猫绘图",
+          "url": "https://fast.catsapi.com",
+          "description": "文生图，图生图，文生视频站点",
+          "icon": "https://www.faviconextractor.com/favicon/fast.catsapi.com"
+        },
+        {
           "id": "site-1752649007053",
           "name": "BeJson",
           "url": "https://www.bejson.com/",
@@ -19,13 +26,6 @@ export const mockData = {
           "url": "https://curlconverter.com/",
           "description": "curl命令转换工具",
           "icon": "/sitelogo/curlconverter.com.ico"
-        },
-        {
-          "id": "site-1774601326477",
-          "name": "猫猫绘图",
-          "url": "https://fast.catsapi.com",
-          "description": "文生图，图生图，文生视频站点",
-          "icon": "https://www.faviconextractor.com/favicon/fast.catsapi.com"
         },
         {
           "id": "linux-do",
@@ -47,6 +47,13 @@ export const mockData = {
           "url": "https://github.com",
           "description": "代码托管平台",
           "icon": "/sitelogo/github.com.ico"
+        },
+        {
+          "id": "site-1778929068955",
+          "name": "猫图 AI",
+          "url": "https://catsapi.com",
+          "description": "AI 生图网站，每日有免费额度，Image2 3 分/张",
+          "icon": "https://www.faviconextractor.com/favicon/catsapi.com"
         }
       ]
     },
